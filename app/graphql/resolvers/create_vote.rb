47ee315236c07de99272c4ced0297744b5a94db9
Felipe Roveran <feliperoveran@hotmail.com>
2018-01-26 14:57:57 -0200

    add create_vote mutation

---
+++ app/graphql/resolvers/create_vote.rb
@@ -0,0 +1,12 @@
+class Resolvers::CreateVote < GraphQL::Function
+  argument :linkID, !types.ID
+
+  type Types::VoteType
+
+  def call(_obj, args, ctx)
+    Vote.create!(
+      link: Link.find_by(id: args[:linkID]),
+      user: ctx[:current_user]
+    )
+  end
+end
