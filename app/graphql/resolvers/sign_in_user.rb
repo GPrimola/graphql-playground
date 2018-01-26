2aaa2b24debafc56912315bf65836456b289f67b
Felipe Roveran <feliperoveran@hotmail.com>
2018-01-26 00:12:36 -0200

    add user authentication

--- app/graphql/resolvers/sign_in_user.rb
+++ app/graphql/resolvers/sign_in_user.rb
@@ -8,7 +8,7 @@ class Resolvers::SignInUser < GraphQL::Function
     field :user, !Types::UserType
   end
 
-  def call(_obj, args, _ctx)
+  def call(_obj, args, ctx)
     signup_data = args[:signupData]
 
     return unless signup_data
@@ -24,7 +24,9 @@ class Resolvers::SignInUser < GraphQL::Function
     crypt = ActiveSupport::MessageEncryptor
       .new(Rails.application.secrets.secret_key_base.byteslice(0..31))
 
-    token = crypt.encrypt_and_sign("user-id:#{ user.id  }")
+    token = crypt.encrypt_and_sign("user-id:#{ user.id }")
+
+    ctx[:session][:token] = token
 
     OpenStruct.new(user: user, token: token)
   end
