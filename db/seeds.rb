f635553b1cd28db456df6122ffb34cce09c455a4
Felipe Roveran <feliperoveran@hotmail.com>
2018-01-26 14:57:51 -0200

    add user and link seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+3.times do |n|
+  User.create(
+    name: "User#{n}",
+    email: "useremail#{n}@example.com",
+    password: "secure-password#{n}"
+  )
+end
+
+10.times do |n|
+  Link.create(
+    url: "path-to-link-#{n}",
+    description: "This is the link ##{n}",
+    user: User.order('RANDOM()').first
+  )
+end
