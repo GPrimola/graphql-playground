4a965144636e8626f29caaa639355bde77ca5678
Felipe Roveran <feliperoveran@hotmail.com>
2018-01-26 22:04:48 -0200

    add pagination to LinkSearch

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'graphql', '~> 1.7.4'
 gem 'bcrypt', '~> 3.1.11'
 gem 'ransack'
+gem 'kaminari'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/graphql/resolvers/link_search.rb
+++ app/graphql/resolvers/link_search.rb
@@ -8,9 +8,21 @@ class Resolvers::LinkSearch < GraphQL::Function
     argument :url_cont, types.String
   end
 
-  argument :filter, LinkFilter
+  LinkPagination = GraphQL::InputObjectType.define do
+    name 'LinkPagination'
 
-  def call(obj, args, ctx)
-    Link.ransack(args[:filter].to_h).result
+    argument :page, types.Int
+    argument :per_page, types.Int
+  end
+
+  argument :filter, LinkFilter
+  argument :pagination, LinkPagination
+
+  def call(_obj, args, _ctx)
+    search_params = args[:filter].to_h
+    page = args[:pagination][:page]
+    per_page = args[:pagination][:per_page]
+
+    Link.ransack(search_params).result.page(page).per(per_page)
   end
 end
